68a62c50c9376b1c7b99c02309cf90f07d659a82
Rahul Ahire <ahirerahul21@gmail.com>
2020-05-27 00:09:44 +0530

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state={
+    profileImg:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+  }
+  imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () =>{
+      if(reader.readyState === 2){
+        this.setState({profileImg: reader.result})
+      }
+    }
+    reader.readAsDataURL(e.target.files[0])
+  };
+	render() {
+    const { profileImg} = this.state
+		return (
+			<div className="page">
+				<div className="container">
+					<h1 className="heading">Add your Image</h1>
+					<div className="img-holder">
+						<img src={profileImg} alt="" id="img" className="img" />
+					</div>
+					<input type="file" accept="image/*" name="image-upload" id="input" onChange={this.imageHandler} />
+					<div className="label">
+          <label className="image-upload" htmlFor="input">
+						<i className="material-icons">add_photo_alternate</i>
+						Choose your Photo
+					</label>
+          </div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
